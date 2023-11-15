84cb18433bca223086f89fb0c44ae86fd9088a48
Levin Bolbas <levinbolbas@gmail.com>
2023-11-15 18:57:55 +0100

    added insert_block function

--- class/Board.py
+++ class/Board.py
@@ -2,13 +2,33 @@
 # import block
 
 
-class Field():
+class Board():
     matrix: int
 
     def __init__(self):
         self.matrix = [[None for _ in range(20)] for _ in range(20)]
+        
 
 
-field = Field()
+    # @ Levin Bolbas
+    def insert_block(self, row: int, column: int, block):
 
-print(field)
+
+            for i in range(len(block)):
+                for j in range(len(block[i])):
+                    
+                    if self.matrix[row + i][column + j] != None:
+                            return "Move not possible!"
+
+            for i in range(len(block)):
+                for j in range(len(block[i])):
+                            self.matrix[row + i][column + j] = block[i][j]
+            
+            return "Movie is possible!"
+                    
+
+
+
+
+
+field = Board()
--- class/Player.py
+++ class/Player.py
@@ -1,4 +1,3 @@
-# @ Lukas Buser, Levin Bolbas
 import Board
 
 class Player(Board):
@@ -6,14 +5,16 @@ class Player(Board):
     color: str
     blocks: dict
 
+    # @ Lukas Buser, Levin Bolbas
     def __init__(self, color):
         self.color = color
         self.blocks = {1: [[True]], 2: [[True,True]], 3: [[True,True],[False,True]], 4: [[True,True,True]]}
 
-
+    # @ Lukas Buser, Levin Bolbas
     def player_insert(self, block, position):
         self.board_insert(block, position)
         self.blocks.pop(block)
 
+    
 
 
