18a3ec1962d2db0ab23d9ecdf9f2478bc5d2b661
Leon-Ams <92448233+Leon-Ams@users.noreply.github.com>
2023-11-20 10:38:10 +0100

    Implemented board_insert, show_board

--- class/Board.py
+++ class/Board.py
@@ -1,13 +1,12 @@
+from Block import Block
+
 class Board():
     matrix: int
-
 
     #@Leon Ams, Sercan Berkpinar, Lukas Buser
     def __init__(self):
         self.matrix = [[None for _ in range(20)] for _ in range(20)]
         
-
-
     # @ Levin Bolbas
     def is_move_valid(self, row: int, column: int, block):
 
@@ -23,7 +22,25 @@ class Board():
             #                 self.matrix[row + i][column + j] = block[i][j]
             
             return True
-                    
+    
+    #@Leon Ams
+    def board_insert(self, block: Block, row: int, col: int):
+        for idx_row, x_row in enumerate(block.block_matrix):
+            for idx_col, val in enumerate(x_row):
+                self.matrix[row + idx_row][col + idx_col] = val  
+
+    #@Leon Ams, Sercan Berkpinar
+    def show_board(self):
+        for x_row in self.matrix:
+            row = f""
+            for y in x_row:
+                cell_value = str(y) if y is not None else ""
+                if len(cell_value) < 5:
+                    cell_value = cell_value.ljust(5)
+                row += f"  {cell_value} |"
+            print(row)
+            print("_________" * 20 + "\n")
+
 
 
 
