1d4bc9a4ccaee4467f61358234631ae16729bfed
Leon-Ams <92448233+Leon-Ams@users.noreply.github.com>
2023-11-20 10:40:29 +0100

    Implemented show_remaining_blocks and refactored class

--- class/Player.py
+++ class/Player.py
@@ -1,19 +1,31 @@
+from Board import Board
+from Block import Block
+
 class Player():
 
     color: str
-    blocks: dict
+    blocks: dict[int, Block]
 
     # @ Lukas Buser, Levin Bolbas
-    def __init__(self, color):
+    def __init__(self, color, board: Board, blocks = None):
         self.color = color
-        self.blocks = {1: [[True]], 2: [[True,True]], 3: [[True,True],[False,True]], 4: [[True,True,True]]}
-
-
+        self.board = board
+        if not blocks:
+            self.blocks = Block.generate_block_set(color)
+        
     # @ Lukas Buser, Levin Bolbas
-    def player_insert(self, block, position):
-        self.board_insert(block, position)
-        self.blocks.pop(block)
+    def player_insert(self, block_idx: int, row: int, col: int):
+        self.board.board_insert(self.blocks[block_idx], row, col)
+        self.blocks.pop(block_idx)
 
-    
+    #@Leon Ams
+    def show_remaining_blocks(self):
+        for idx, block in self.blocks.items():
+            print(f"\n\n Block index {idx}:\n")
+            block.show_block()
 
-
+#board = Board()
+#player = Player("red", board)
+#player.player_insert(3, 1, 2)
+#player.board.show_board()
+#player.show_remaining_blocks()
