a7d3017b413fdb17ff0644afb02f21def75d7737
Leon-Ams <92448233+Leon-Ams@users.noreply.github.com>
2023-11-20 10:37:14 +0100

    Implemented help functions (color_matrix, show_block, generate_block_set, etc.)

--- class/Block.py
+++ class/Block.py
@@ -1,14 +1,16 @@
-block_dict = {1: [[True]], 2: [[True,True]], 3: [[True,True],[False,True]], 4: [[True,True,True]]}
+from copy import deepcopy
+
+BLOCKS_MATRIX = {1: [[True]], 2: [[True,True]], 3: [[True,True],[None,True]], 4: [[True,True,True]]}
 
 class Block():
     color: str
-    block_matrix: int
+    block_matrix: list[list[None, str]]
 
     #@Leon Ams, Sercan Berkpinar, Lukas Buser
-    def __init__(self, block_index, color):
-        self.block_matrix = block_dict[block_index]
+    def __init__(self, matrix: list[list[bool]], color):
         self.color = color
-
+        self.block_matrix = matrix
+        self.color_matrix()
 
     #@Leon Ams, Sercan Berkpinar, Lukas Buser
     def rotate(self):
@@ -19,7 +21,34 @@ class Block():
                 rotated_matrix[j][len_rows - 1 - i] = self.block_matrix[i][j]
         self.block_matrix = rotated_matrix
 
-
     #@Leon Ams, Sercan Berkpinar, Lukas Buser
     def reflect(self):
         self.block_matrix = [row[::-1] for row in self.block_matrix]
+    
+    #@Leon Ams
+    def color_matrix(self):
+        for idx_row, x_row in enumerate(self.block_matrix):
+            for idx_col, val in enumerate(x_row):
+                if val:
+                    self.block_matrix[idx_row][idx_col] = self.color
+    
+    #@Leon Ams, Sercan Berkpinar
+    def show_block(self):
+        for row in self.block_matrix:
+            row_str = ""
+            for y in row:
+                cell_value = str(y) if y else ""
+                if len(cell_value) < 5:
+                    cell_value = cell_value.ljust(5)
+                row_str += f"  {cell_value} |"
+            print("_" * len(row_str) + "\n")
+            print(row_str)
+        print("_" * len(row_str) + "\n")
+
+    #@Leon Ams  - generates full dict of blocks
+    @staticmethod
+    def generate_block_set(color: str, blocks_set = BLOCKS_MATRIX):
+        blocks = {}
+        for idx, matrix in blocks_set.items():
+            blocks[idx] = deepcopy(Block(matrix, color))
+        return blocks
